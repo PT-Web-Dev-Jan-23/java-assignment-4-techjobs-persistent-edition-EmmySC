bd37ab92d888a8b7388f205cb3e56c6e7ab7da22
EmmyC <esc@Emilys-MBP-2.attlocal.net>
2023-06-04 21:07:21 -0500

    Part 2 complete

--- queries.sql
+++ queries.sql
@@ -1,9 +1,14 @@
--- Part 1: Test it with SQL
-SHOW COLUMNS FROM job;
+-- Part 1: Test it with SQL --
+--SHOW COLUMNS FROM job;
+id (int, PK),
+name (varchar),
+employer (varchar),
+skills (varchar);
 
--- Part 2: Test it with SQL
-SELECT * FROM employer WHERE (location="St.Louis City");
+-- Part 2: Test it with SQL --
+--SELECT * FROM employer WHERE (location="St.Louis City");
+SELECT name
+FROM employer
+WHERE location = "St. Louis City";
 
--- Part 3: Test it with SQL
 
--- Part 4: Test it with SQL
