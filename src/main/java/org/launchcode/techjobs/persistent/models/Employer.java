b08c9d145d949950efea1f56ce60ef4df567071d
EmmyC <esc@Emilys-MBP-2.attlocal.net>
2023-06-04 22:30:00 -0500

    Part 3 added jobs Field to Employer.java & updated Job Model

--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -2,8 +2,12 @@ package org.launchcode.techjobs.persistent.models;
 
 
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
 
 //In addition to the fields inherited from AbstractEntity, Employer should have a string field for location.
 //Add the field for location with validation that ensures it is not empty and has a reasonable length.
@@ -14,6 +18,15 @@ import javax.validation.constraints.Size;
 @Entity
 public class Employer extends AbstractEntity {
 
+
+//Within Employer, add a private property jobs of type List<Job> and initialize it to an empty ArrayList.
+//After setting up Job class to work w/ Employer objects, this list will represent the list of all items in a given job.
+//Use the @OneToMany and @JoinColumn annotations on the jobs list in Employer to declare the relationship between
+//data tables. Recall that this annotation needs a name parameter.
+    @OneToMany
+    @JoinColumn(name = "employer_id")
+    private List<Job> jobs = new ArrayList<>();
+
     @NotBlank(message = "a LOCATION is required")
     @Size(max = 100, message = "the LOCATION must be < 100 characters in length")
     private String location;
@@ -23,6 +36,15 @@ public class Employer extends AbstractEntity {
         this.location = location;
     }
 
+//NEEDED? best practice?//
+//    public List<Job> getJobs() {
+//        return jobs;
+//    }
+//
+//    public void setJobs(List<Job> jobs) {
+//        this.jobs = jobs;
+//    }
+
     public String getLocation() {
         return location;
     }
@@ -30,4 +52,6 @@ public class Employer extends AbstractEntity {
     public void setLocation(String location) {
         this.location = location;
     }
+
+
 }
--- src/main/java/org/launchcode/techjobs/persistent/models/Job.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Job.java
@@ -1,51 +1,68 @@
 package org.launchcode.techjobs.persistent.models;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+
+//Since the Job model class has id and name fields, it too can inherit from AbstractEntity.
+//Update the class definition of Job to extend AbstractEntity. Remove the redundant fields from Job.
 
 @Entity
-public class Job{
+public class Job extends AbstractEntity {
 
-    @Id
-    @GeneratedValue
-    private int id;
+//Replace the type of the field employer to be of type Employer.
+// Add the @ManyToOne annotation on the field employer
+    @ManyToOne
+    private Employer employer;
 
-    private String name;
+    @ManyToMany
+    private List<Skill> skills = new ArrayList<>();
 
-    private String employer;
-    private String skills;
+//
+
+//    @Id
+//    @GeneratedValue
+//    private int id;
+//
+//    private String name;
+//
+//    private String employer;
+//    private String skills;
 
     public Job() {
     }
 
-    public Job(String anEmployer, String someSkills) {
+//Need to refactor the affected constructor that uses this field.
+    public Job(Employer anEmployer, List<Skill> someSkills) { //public Job(String anEmployer, String someSkills)
         super();
         this.employer = anEmployer;
         this.skills = someSkills;
     }
 
-    // Getters and setters.
+//Need to refactor the affected getter and setter that use this field
 
-    public String getName() {
-        return name;
-    }
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmployer() {
+    public Employer getEmployer() {  //public String getEmployer()
         return employer;
     }
 
-    public void setEmployer(String employer) {
+    public void setEmployer(Employer employer) {  //public void setEmployer(String employer)
         this.employer = employer;
     }
 
-    public String getSkills() {
+    public List<Skill> getSkills() {  //public String getSkills()
         return skills;
     }
 
-    public void setSkills(String skills) {
+    public void setSkills(List<Skill> skills) {  //public void setSkills(String skills)
         this.skills = skills;
     }
 }
